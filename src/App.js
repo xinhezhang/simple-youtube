f4afda3d0cf3e70471321030b9bba45f513fd80e
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 01:09:04 -0800

    implement SearchBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import Header from './components/header';
 import SearchBar from './components/search_bar';
@@ -7,6 +9,12 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
     render() {
         return (
             <div className="App">
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: '',
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(event) {
+        this.setState({
+            keyword: event.target.value,
+        });
+    }
     render() {
         return (
-            <div>
-                <input />
+            <div className="search-bar">
+                <input
+                    onChange={this.onInputChange}
+                    value={this.state.keyword}
+                />
+                {this.state.keyword}
             </div>
         );
     }
