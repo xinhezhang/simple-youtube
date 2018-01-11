6106a26d57625c771c0e63c2d5ea6a8a995db48f
xinhezhang <xinhezhang@hotmail.com>
2018-01-11 01:19:58 -0800

    implement VideoListItem component

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 class VideoListItem extends Component {
     render() {
+        const video = this.props.video;
+        const imageUrl = video.snippet.thumbnails.default.url;
+
         return (
-            <li>
-                <div>
-                    <div></div>
-                    <div></div>
+            <li className="list-group-item">
+                <div className="video-list media">
+                    <div className="media-left">
+                        <img className="media-object" src={imageUrl} />
+                    </div>
+                    <div className="media-body">
+                        <div className="media-heading">
+                            {video.snippet.title}
+                        </div>
+                    </div>
                 </div>
             </li>
         );
